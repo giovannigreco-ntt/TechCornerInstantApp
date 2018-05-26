37497bec14fec727b851c1c3875ef5415ad97c7e
Greco Giovanni <Giovanni.Greco@nttdata.com>
2018-05-26 18:35:10 +0200

    WIP

--- TCApp/featureRecordVideo/src/main/java/com/nttdata/tcapp/featureRecordVideo/RecordVideoActivity.java
+++ TCApp/featureRecordVideo/src/main/java/com/nttdata/tcapp/featureRecordVideo/RecordVideoActivity.java
@@ -1,17 +1,45 @@
 package com.nttdata.tcapp.featureRecordVideo;
 
 import android.Manifest;
+import android.content.ContentProvider;
+import android.content.ContentProviderClient;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.icu.util.Output;
 import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.util.IOUtils;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Date;
 
 public class RecordVideoActivity extends AppCompatActivity {
 
@@ -20,10 +48,14 @@ public class RecordVideoActivity extends AppCompatActivity {
 
     private TextView consoleTV;
 
+    private StorageReference mStorageRef;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_record_video);
+
+        mStorageRef = FirebaseStorage.getInstance().getReference();
 
         initUIRefs();
 
@@ -62,12 +94,16 @@ public class RecordVideoActivity extends AppCompatActivity {
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
 
+        boolean granted = true;
+
         for (int i=0; i<permissions.length; i++) {
-            if(permissions[i].equals(Manifest.permission.CAMERA)) {
-                if (grantResults[i] == PackageManager.PERMISSION_GRANTED) {
-                    startVideoRecording();
-                }
+            if (grantResults[i] != PackageManager.PERMISSION_GRANTED) {
+                granted = false;
             }
+        }
+
+        if (granted) {
+            startVideoRecording();
         }
 
 
@@ -79,9 +115,87 @@ public class RecordVideoActivity extends AppCompatActivity {
             if (requestCode == PICK_VIDEO_REQUEST) {
                 log("Video recorded");
                 Uri selectedVideoUri = data.getData();
-                //TODO: upload video to firebase
+
+                try {
+                    uploadData(getContentResolver().openInputStream(selectedVideoUri));
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
             }
         }
+    }
+
+
+
+    private void uploadData(Uri videoUri) {
+
+
+        StorageReference riversRef = mStorageRef.child("video_"+new Date());
+
+        riversRef.putFile(videoUri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+
+                        log("VIDEO UPLOADED :)");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        // ...
+                        log("VIDEO UPLOAD FAILED :(");
+                    }
+                });
+    }
+
+    private void uploadData(InputStream inputStream) {
+
+        File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/video");
+
+        try {
+
+            OutputStream output = new FileOutputStream(file);
+            byte[] buffer = new byte[4 * 1024]; // or other buffer size
+            int read;
+
+            while ((read = inputStream.read(buffer)) != -1) {
+                output.write(buffer, 0, read);
+            }
+
+            output.flush();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                inputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+
+
+        StorageReference riversRef = mStorageRef.child("video_"+new Date());
+
+        riversRef.putFile(Uri.fromFile(file))
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        log("VIDEO UPLOADED :)");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        log("VIDEO UPLOAD FAILED :(");
+                    }
+                });
     }
 
     private void log(String log) {
@@ -89,4 +203,6 @@ public class RecordVideoActivity extends AppCompatActivity {
         consoleTV.setText(log);
 
     }
+
+
 }
